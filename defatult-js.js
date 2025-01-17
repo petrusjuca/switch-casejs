b7ce3ec5274408d30cedc96c73f731e8dbb4d038
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-16 21:16:41 -0300

    Create defatult-js.js
    O default em uma estrutura switch serve como um "valor de fallback" ou uma opção que será executada quando nenhum dos cases corresponder ao valor da expressão do switch.
    
    O default é opcional e pode ser colocado em qualquer lugar dentro do switch, mas geralmente é colocado no final para indicar que é a opção padrão.

---
+++ defatult-js.js
@@ -0,0 +1,14 @@
+switch (opcao) {
+    case 1:
+        printf("Opção 1 escolhida\n");
+        break;
+    case 2:
+        printf("Opção 2 escolhida\n");
+        break;
+    case 3:
+        printf("Opção 3 escolhida\n");
+        break;
+    default:
+        printf("Opção inválida\n");
+        break;
+}
